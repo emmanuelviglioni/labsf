1fa3fcced50c76d596380425be637618ebe3eb39
Emmanuel <emmanuelviglioni0@gmail.com>
2024-05-06 00:43:01 -0300

    Modify TaskController

--- roteiro01 2/src/main/java/com/ladesoft/roteiro01/controller/TaskController.java
+++ roteiro01 2/src/main/java/com/ladesoft/roteiro01/controller/TaskController.java
@@ -1,14 +1,67 @@
 package com.ladesoft.roteiro01.controller;
 
+import com.ladesoft.roteiro01.entity.Task;
+import com.ladesoft.roteiro01.service.TaskService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
 @RestController
+@RequestMapping("/task")
 public class TaskController {
-    @GetMapping("/task")
+
+    private final TaskService m_taskService;
+    public TaskController(TaskService p_taskService)
+    {
+        m_taskService = p_taskService;
+    }
+
+    @GetMapping(value = {"/listAll", ""})
     @Operation(summary = "Lista todas as tarefas da lista")
-    public String listAll() {
-        return "Listar todas as tasks";
+    public ResponseEntity<List<Task>> listAll()
+    {
+        try
+        {
+            List<Task> v_listOfTask = m_taskService.getAllTask();
+            if(v_listOfTask == null)
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(v_listOfTask, HttpStatus.OK);
+        }
+        catch(Exception ex)
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage());
+        }
+    }
+
+    @PostMapping(value= {"/create",""})
+    @Operation(summary = "Cria nova tarefa")
+    public ResponseEntity<String> create(@ModelAttribute Task task)
+    {
+        try
+        {
+            return new ResponseEntity<>(m_taskService.createTask(task),HttpStatus.OK);
+        }
+        catch(Exception ex)
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage());
+        }
+    }
+
+    @PutMapping(value = {"/setTaskAsComplete","/setTaskAsPending",""})
+    @Operation(summary = "Muda o status da tarefa")
+    public ResponseEntity<String> edit(@ModelAttribute Task task)
+    {
+        try
+        {
+            return new ResponseEntity<>(m_taskService.editTask(task),HttpStatus.OK);
+        }
+        catch(Exception ex)
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage());
+        }
     }
 }
